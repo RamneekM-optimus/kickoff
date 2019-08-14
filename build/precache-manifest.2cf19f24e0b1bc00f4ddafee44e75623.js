13443c820c7ae3f6dfd6ca581bc8deb57b52a4c4
Ramneek Marwaha <ramneek.marwaha@OPTIMUSDOM.COM>
2019-08-14 15:11:41 +0530

    hello

---
+++ build/precache-manifest.2cf19f24e0b1bc00f4ddafee44e75623.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a31d557d86f2dafc7bc013e940f68e4",
+    "url": "/kickoff/index.html"
+  },
+  {
+    "revision": "b2812add89c9151b1007",
+    "url": "/kickoff/static/css/main.b8d3a8d7.chunk.css"
+  },
+  {
+    "revision": "4390ffe199841272155d",
+    "url": "/kickoff/static/js/2.51675ca7.chunk.js"
+  },
+  {
+    "revision": "b2812add89c9151b1007",
+    "url": "/kickoff/static/js/main.ddc428ef.chunk.js"
+  },
+  {
+    "revision": "c8c55e6a071c61677d05",
+    "url": "/kickoff/static/js/runtime~main.3be78077.js"
+  },
+  {
+    "revision": "121fb69a596fe32f1e77c4dec1c6ac35",
+    "url": "/kickoff/static/media/add.121fb69a.svg"
+  },
+  {
+    "revision": "0eac654aa7cd37ccfb39dca9a6176397",
+    "url": "/kickoff/static/media/minus.0eac654a.svg"
+  },
+  {
+    "revision": "4fcc44dcafae049527e73a065b72b4b3",
+    "url": "/kickoff/static/media/web.4fcc44dc.config"
+  }
+]);
